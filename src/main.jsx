e7e7a1d8fcc5bfa293548008e107bfa9032baee5
Nithin Daniel <nithindaniel2018@gmail.com>
2024-03-30 00:06:31 +0530

    updaed index.html

--- src/main.jsx
+++ src/main.jsx
@@ -6,20 +6,30 @@ import {
   Route,
   Link,
 } from "react-router-dom";
+import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
+import Todo from "./pages/Todo/Todo";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
       <div>
-        <h1>Hello World</h1>
-        <Link to="about">About Us</Link>
+        <h1>Home Page</h1>
       </div>
     ),
   },
   {
-    path: "about",
-    element: <div>About</div>,
+    path: "signup",
+    element:<Signup/>,
+  },
+  {
+    path: "login",
+    element:<Login/>,
+  },
+  {
+    path: "todo",
+    element:<Todo/>,
   },
 ]);
 
